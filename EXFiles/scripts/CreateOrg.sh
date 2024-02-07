9326c1a7f57af7248bbd3f2e5fd87ee360fcee96
yujinK <yuujinius@gmail.com>
2024-02-07 11:14:50 +0900

    initial

--- EXFiles/scripts/CreateOrg.sh
+++ EXFiles/scripts/CreateOrg.sh
@@ -1,6 +1,6 @@
 # Execute in Mac using: ./EXFiles/scripts/CreateOrg.sh
 echo "*** Creating scratch Org..."
-sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soDEX602 -d 30
+sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soDEX602_1 -d 30
 echo "*** Opening scratch Org..."
 sfdx force:org:open
 echo "*** Pushing metadata to scratch Org..."
